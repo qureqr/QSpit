d18a2e465a2be152683f7b30579782ff6107c0de
СвятоСвят <qureqr@gmail.com>
2024-02-12 01:11:47 +0200

    Add fart logic

--- src/main/java/org/example/sperma/jidki/SpitPlugin.java
+++ src/main/java/org/example/sperma/jidki/SpitPlugin.java
@@ -1,15 +1,13 @@
 package org.example.sperma.jidki;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Particle;
-import org.bukkit.Sound;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LlamaSpit;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.ShulkerBullet;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
@@ -75,13 +73,20 @@ public class SpitPlugin extends JavaPlugin implements CommandExecutor {
 
             player.sendMessage("харчёк полетел");
         } else if (label.equalsIgnoreCase("fart")) {
-
-
             Location playerLocation = player.getLocation();
-            player.getWorld().spawnParticle(Particle.EXPLOSION_NORMAL, playerLocation, 100, 0.5, 0.5, 0.5, 0.1);
-            player.getWorld().playSound(playerLocation, Sound.ENTITY_GENERIC_EXPLODE, 1.0f, 1.0f);
+            World world = player.getWorld();
+
+            Location spawnLocation = playerLocation.clone().subtract(0, 1.5, 0);
+
+            ShulkerBullet shulkerBullet = (ShulkerBullet) world.spawnEntity(spawnLocation, EntityType.SHULKER_BULLET);
+            shulkerBullet.setShooter(player);
+
+            Vector direction = player.getLocation().getDirection().multiply(-2); // Отрицательный множитель для направления назад
+            shulkerBullet.setVelocity(direction);
+
             player.sendMessage("уфф пернул знатно");
         }
         return true;
+
     }
 }
